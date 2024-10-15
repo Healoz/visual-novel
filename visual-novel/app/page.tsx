6d533d29090e73b8c66ef2fe65d93ca191555327
Healoz <lauren.easter59@gmail.com>
2024-10-15 12:11:50 +1100

    set up nextjs project

---
+++ visual-novel/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl p-3">Story Creator</h1>
+      <div>
+        <h2>Hey there bestie</h2>
+      </div>
+    </div>
+  );
+}
