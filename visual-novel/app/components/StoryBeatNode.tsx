dcafc266aa70c81b9527967e968920bb49bf9624
Healoz <lauren.easter59@gmail.com>
2024-11-01 21:05:21 +1100

    started to make dynamic need to create a useContext for storyBeat functions or just move them over

--- visual-novel/app/components/StoryBeatNode.tsx
+++ visual-novel/app/components/StoryBeatNode.tsx
@@ -2,21 +2,9 @@ import React, { useCallback } from "react";
 import { Handle, NodeProps, Position, Node } from "@xyflow/react";
 import { Character, StoryBeat } from "../types";
 
-interface StoryBeatNodeData {
-  storyBeat: StoryBeat;
-  characters: Character[];
-  storyBeats: StoryBeat[];
-  createNewDialogue: (storyBeatId: string) => void;
-  createNewChoice: (storyBeatId: string) => void;
-  updateStoryBeat: (storyBeatId: string, updatedStoryBeat: StoryBeat) => void;
-  deleteStoryBeat: (id: string) => void;
-  deleteDialogue: (storyBeat: StoryBeat, dialogueId: string) => void;
-  deleteChoice: (storyBeat: StoryBeat, choiceId: string) => void;
-}
-
 // creating custom node type that extends Node
 
-type StoryBeatNode = Node<
+export type StoryBeatNode = Node<
   {
     storyBeat: StoryBeat;
     characters: Character[];
@@ -32,15 +20,27 @@ type StoryBeatNode = Node<
 >;
 
 const StoryBeatNode = (props: NodeProps<StoryBeatNode>) => {
+  const data = props.data;
+  const {
+    storyBeat,
+    characters,
+    storyBeats,
+    createNewDialogue,
+    createNewChoice,
+    updateStoryBeat,
+    deleteStoryBeat,
+    deleteDialogue,
+    deleteChoice,
+  } = data;
   return (
     <>
       <Handle type="target" position={Position.Top} />
       <div className="border p-3 bg-white flex gap-3">
+        <h4>{}</h4>
         <label htmlFor="text">Text:</label>
         <input id="text" name="text" className="nodrag bg-gray-100 p-2" />
       </div>
       <Handle type="source" position={Position.Bottom} id="a" />
-      <Handle type="source" position={Position.Bottom} id="b" />
     </>
   );
 };
